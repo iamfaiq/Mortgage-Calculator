7ecdb176df2f8b91f9a7cfb0c52b4f8c612d63c0
iamfaiq <43803954+iamfaiq@users.noreply.github.com>
2025-07-02 00:38:50 -0400

    Update Main.java
    Extracted the formatting and printing logic from the main method into private methods for monthly amount and remaining balance

--- src/Main.java
+++ src/Main.java
@@ -10,22 +10,34 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         int principal = (int)readNumber("Principal($1k - $1M): ", scanner, 1000, 1_000_000);
         float interestRate = (float)readNumber("Annual Interest Rate: ", scanner, 1, 30);
         byte years = (byte)readNumber("Period(Years): ", scanner, 1, 30);
 
-        System.out.println();
-        String monthlyMortgage = NumberFormat.getCurrencyInstance().format(calculateMortgage(principal,interestRate,years));
-        System.out.println("Your monthly amount due will be: "+monthlyMortgage);
+        printMortgage(principal, interestRate, years);
 
+        printBalance(years, principal, interestRate);
+
+    }
+
+    private static void printMortgage(int principal, float interestRate, byte years) {
+        double mortgage = calculateMortgage(principal, interestRate, years);
+        String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println();
-        System.out.println("Remaining amount due after each month:");
-        for (short i = 1; i<=years*MONTHS_IN_A_YEAR;i++){
-            String balance = NumberFormat.getCurrencyInstance().format(calculateBalance(principal,interestRate,years,i));
-            System.out.println(i+") "+balance);
+        System.out.println("Monthly Mortgage");
+        System.out.println("----------------");
+        System.out.println("Monthly amount due will be: "+formattedMortgage);
+    }
+
+    private static void printBalance(byte years, int principal, float interestRate) {
+        System.out.println();
+        System.out.println("Remaining Balance after each payment");
+        System.out.println("------------------------------------");
+        for(short month = 1; month <= years *MONTHS_IN_A_YEAR; month++){
+            double balance = calculateBalance(principal, interestRate, years, month);
+            String remainingBalance = NumberFormat.getCurrencyInstance().format(balance);
+            System.out.println(month +") "+remainingBalance);
         }
-
     }
 
     public static double readNumber(String prompt, Scanner scanner, int min, int max){
